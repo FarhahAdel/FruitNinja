d89132e11443b290d43fb355bb8f8e0f0a0827de
yosraemad <yosraemad14@gmail.com>
2020-04-12 18:06:52 +0200

    Game page UI , awaiting css

---
+++ src/fruitNinja/views/pages/GameController.java
@@ -0,0 +1,21 @@
+package fruitNinja.views.pages;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class GameController {
+    @FXML
+    private Button pauseButton;
+    @FXML
+    private Label scoreLabel;
+    @FXML
+    private Label timerLabel;
+    @FXML
+    private Label livesLabel;
+
+
+    public void pauseButtonClicked(ActionEvent actionEvent) {
+    }
+}
