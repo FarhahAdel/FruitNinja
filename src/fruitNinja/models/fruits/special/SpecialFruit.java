736888a32eeb1ce0a7212e7aafdc769fe32d5e49
Omar Alaa <omar.skillful@gmail.com>
2020-04-30 00:21:31 +0200

    Special fruits now adds 3 points

--- src/fruitNinja/models/fruits/special/SpecialFruit.java
+++ src/fruitNinja/models/fruits/special/SpecialFruit.java
@@ -2,6 +2,8 @@ package fruitNinja.models.fruits.special;
 
 import fruitNinja.models.audios.AudioController;
 import fruitNinja.models.fruits.Fruit;
+import fruitNinja.models.guiUpdate.ControlsUpdater;
+import fruitNinja.models.guiUpdate.ControlsUpdaterSingleton;
 
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
@@ -9,12 +11,14 @@ import java.io.IOException;
 
 public class SpecialFruit extends Fruit {
     private AudioController audioController = new AudioController();
+    private final int specialFruitsPoints = 3;
 
     @Override
     public void slice()
     {
         setImage(getImageAfterSlicing());
         audioController.start("special");
+        ControlsUpdaterSingleton.getInstance().sliceOrdinaryFruit(specialFruitsPoints);
         setSliced(true);
     }
 
