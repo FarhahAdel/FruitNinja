4ca23382b4790df666d550522a723ca19680f119
Yosra Emad <yosraemad14@gmail.com>
2020-05-01 06:11:44 +0200

    Pause now works

--- src/fruitNinja/models/gameModes/Stratgies/BaseStrategy.java
+++ src/fruitNinja/models/gameModes/Stratgies/BaseStrategy.java
@@ -1,11 +1,13 @@
 package fruitNinja.models.gameModes.Stratgies;
 
+import fruitNinja.models.gameLogic.GamePlayActions;
 import fruitNinja.models.gameModes.StrategyType;
 import fruitNinja.utils.timerTasks.Round;
 import javafx.scene.canvas.Canvas;
 
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
 
 public class BaseStrategy implements GameStrategy{
     final StrategyType strategyType;
@@ -17,13 +19,38 @@ public class BaseStrategy implements GameStrategy{
     }
     @Override
     public void initGame(Canvas canvas) {
-        TimerTask timerTask = new Round(canvas,strategyType,timer);
         int x = 4500;
-        timer.schedule(timerTask,500,x);
+        timer.schedule(new Round(canvas,strategyType),500,x);
+
+        Timer pauseTimer = new Timer();
+
+        final Boolean[] wasPaused = {false};
+
+        pauseTimer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                if(GamePlayActions.isPaused){
+                    wasPaused[0] = true;
+                    timer.cancel();
+                }
+                else if(wasPaused[0]){
+                    try {
+                        TimeUnit.SECONDS.sleep(2);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    wasPaused[0] = false;
+                    timer = new Timer();
+                    timer.schedule(new Round(canvas,strategyType),500,x);
+                }
+            }
+        },500,500);
     }
+
 
     @Override
     public StrategyType getStrategyType() {
         return this.strategyType;
     }
+
 }
--- src/fruitNinja/utils/timerTasks/Round.java
+++ src/fruitNinja/utils/timerTasks/Round.java
@@ -6,7 +6,6 @@ import fruitNinja.models.gameLogic.GameProperties;
 import fruitNinja.models.gameModes.StrategyType;
 import javafx.scene.canvas.Canvas;
 
-import java.util.Timer;
 import java.util.TimerTask;
 
 public class Round extends TimerTask {
@@ -14,20 +13,23 @@ public class Round extends TimerTask {
     private GamePlayActions gamePlayActions;
     private GameLogic gameLogic;
     private GameProperties gameProperties;
-    private Timer timer;
+    private Boolean wasPaused = false;
 
-    public Round(Canvas canvas, StrategyType strategyType, Timer timer)
+    private Canvas canvas;
+    private StrategyType strategyType;
+
+    public Round(Canvas canvas, StrategyType strategyType )
     {
         gamePlayActions = new GamePlayActions();
         gamePlayActions.setCanvas(canvas);
         gameLogic = new GameLogic(strategyType);
         gameProperties = new GameProperties();
-        this.timer = timer;
+        this.canvas = canvas;
+        this.strategyType = strategyType;
     }
 
     @Override
     public void run() {
-        if(GamePlayActions.isPaused) {timer.cancel(); timer.purge();}
         gameLogic.startRound(gameProperties, gamePlayActions);
     }
 
